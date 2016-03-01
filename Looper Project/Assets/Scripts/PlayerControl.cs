3fffc7970b8ba65ff18cc32016a49742b2c42f7d
illuminatedspace <lizkristinaphillips@gmail.com>
2016-02-29 20:22:06 -0500

    adds current unity project from google drive

---
+++ Looper Project/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	private Rigidbody2D rb; // Declares a variable of type RigidBody2D game component.
+
+
+
+	public float speed = 10f;   // Variable that multiplies the force applied to the rigidbody.
+	public float jumpVelocity = 50f; // Variable determining the sudden vertical force applied to the rigidbody.
+	private Animator MyAnimator;
+
+	public LayerMask groundLayerTest;
+
+	void Start() // This function Attaches this script to the rigidbody that is also a  part of this scripts object.
+	{
+		rb = GetComponent<Rigidbody2D> (); // Gets the Rigidbody2D component that is attached to the same asset as this script.
+		MyAnimator = GetComponent<Animator>();
+	}
+
+
+
+
+
+	void Update () // This function reads in the Vertical/Horizontal input of the player, and applys force to the rigidbody accordingly.
+	{
+
+
+		float moveHorizontal = Input.GetAxis("Horizontal") * speed;
+		var moveVertical = 0f;
+
+
+		if (Input.GetButtonDown ("Jump") && rb.IsTouchingLayers (groundLayerTest)) {
+			moveVertical = jumpVelocity;
+		}
+
+		if (!rb.IsTouchingLayers (groundLayerTest)) {
+			MyAnimator.SetBool ("Jump", true);
+		} 
+
+		else {
+			MyAnimator.SetBool ("Jump", false);
+		}
+
+		if (moveHorizontal != 0) {
+			MyAnimator.SetBool ("Running", true);
+		} else
+			MyAnimator.SetBool ("Running", false);
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+		rb.AddForce(movement);
+
+	}
+}
---
+++ Looper Project/Assets/Scripts/PortalBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalBehaviour : MonoBehaviour {
+	
+	// Function for when an object walks into the trigger colider (Should destroy the object).
+	
+	public GameObject targetPosition;
+	
+
+	void OnTriggerEnter2D(Collider2D other) {
+		print ("Hit Trigger");
+		//if(other.name == "Player")
+		other.transform.position = targetPosition.transform.position;
+	}
+	
+}
