edf9976a5a463c6a53e2af35b5cd03581e15bc8e
Greg Johnson <greg93John@gmail.com>
2016-06-13 19:16:30 -0400

    Script fix
    Fixes duplication error with the 'TextScript' in the assets folder.

--- Looper Project/Assets/Scripts/TextScript.cs
+++ Looper Project/Assets/Scripts/TextScript.cs
@@ -3,6 +3,15 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class TextScript : MonoBehaviour {
+    //publc variables
+    public Text text;
+    public bool ColorChange;
+    public bool textChange;
+
+    //private variables
+    private enum LoopState {
+        first, second, third
+        };
 
 	// Use this for initialization
 	void Start () {
