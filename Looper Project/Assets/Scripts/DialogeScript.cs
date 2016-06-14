de447a6a3c72c37e19ac4dd458cd7f46153f512d
Greg Johnson <greg93John@gmail.com>
2016-06-14 04:47:10 -0400

    Text Script Modification
    Edits 'TextScript' and 'DialogeScript' files in the asset folder so that
    the text inside the dialogue box appears and disappears with the
    Dialogue Speech bubble Image.

--- Looper Project/Assets/Scripts/DialogeScript.cs
+++ Looper Project/Assets/Scripts/DialogeScript.cs
@@ -28,11 +28,13 @@ public class DialogeScript : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collider) {
         Debug.Log("Entered collider");
         targetColor.a = 1f;
+        TextScript.colorChange = true;
     }
 
     void OnTriggerExit2D(Collider2D collider) {
         Debug.Log("Exited collider");
         targetColor.a = 0;
+        TextScript.colorChange = false;
     }
 
 }
--- Looper Project/Assets/Scripts/TextScript.cs
+++ Looper Project/Assets/Scripts/TextScript.cs
@@ -5,21 +5,40 @@ using UnityEngine.UI;
 public class TextScript : MonoBehaviour {
     //publc variables
     public Text text;
-    public bool ColorChange;
+    public static bool colorChange;
     public bool textChange;
 
     //private variables
     private enum LoopState {
         first, second, third
         };
+    private Color targetColor;
 
 	// Use this for initialization
 	void Start () {
-	
+        targetColor = text.color;
+        targetColor.a = 0f;
+        text.color = targetColor;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+	    if (colorChange){
+            showText();
+        } else {
+            hideText();
+        }
+    }
+
+    void showText() {
+        targetColor.a = 1f;
+        text.color = targetColor;
+        Debug.Log("Text Should appear!");
+    }
+
+    void hideText() {
+        targetColor.a = 0f;
+        text.color = targetColor;
+        Debug.Log("Text should dissapear!");
+    }
 }
