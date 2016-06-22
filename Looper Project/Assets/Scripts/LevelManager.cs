453ff028c656216e2784ffa49a25d017a27f1dd3
illuminatedspace <anfractua@gmail.com>
2016-06-22 18:23:20 -0400

    Converts the project to 5.3.5f1

---
+++ Looper Project/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManager;
+
+public class LevelManager : MonoBehaviour {
+
+    static LevelManager instance = null;
+    
+
+    // public variables
+    public float autoLoadNextLevelAfter;
+ 
+
+//    void Awake() {
+//        if (instance != null && instance != this)
+//        {
+//            Destroy(gameObject);
+//        } else {
+//            instance = this;
+//            DontDestroyOnLoad(gameObject);
+//        }
+//    }
+
+    void Start(){
+        if(autoLoadNextLevelAfter <= 0){
+            Debug.Log("Auto level disabled");
+        } else{
+            Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+        }
+     }
+
+	public void LoadLevel(string name)  {
+	
+		Debug.Log ("New Level load: " + name);
+        SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest()   {
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+    public void LoadNextLevel() {
+        Debug.Log("Loading next scene");
+        SceneManager.LoadScene(Application.loadedLevel + 1);
+    }
+}
